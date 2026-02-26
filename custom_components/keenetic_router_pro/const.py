de2c14b75e93c0fb2fa5849ba9f0184d1ffd7004
cataseven <52464808+cataseven@users.noreply.github.com>
2026-02-26 14:53:24 +0300

    Add files via upload

---
+++ custom_components/keenetic_router_pro/const.py
@@ -0,0 +1,13 @@
+"""Constants for the Keenetic Router Pro integration."""
+
+DOMAIN = "keenetic_router_pro"
+DEFAULT_PORT = 100
+DEFAULT_SSL = False
+FAST_SCAN_INTERVAL = 10
+SLOW_SCAN_INTERVAL = 60
+PING_SCAN_INTERVAL = 3
+DATA_CLIENT = "client"
+DATA_COORDINATOR = "coordinator"
+DATA_PING_COORDINATOR = "ping_coordinator"
+CONF_TRACKED_CLIENTS = "tracked_clients"
+EVENT_NEW_DEVICE = f"{DOMAIN}_new_device"
